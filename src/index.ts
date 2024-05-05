7f221f143cc7f7a58c78b53111794c1743f7ae9b
pausm99 <pausm99@gmail.com>
2024-05-05 21:33:33 +0200

    basic input output main function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log("Hello via Bun!");
+import * as readlineSync from 'readline-sync';
+
+function main() {
+    while (true) {
+      console.log('\n1. Manage Leagues');
+      console.log('2. Manage Teams');
+      console.log('3. Manage Players');
+      console.log('4. Manage PlayerStats');
+      console.log('5. Exit\n');
+  
+      const choice = readlineSync.question('Enter your choice: ');
+  
+      switch (choice) {
+        case '1':
+          manageLeagues();
+          break;
+        case '2':
+          manageTeams();
+          break;
+        case '3':
+          managePlayers();
+          break;
+        case '4':
+          managePlayerStats();
+          break;
+        case '5':
+          console.log('Exiting program.');
+          return;
+        default:
+          console.log('Invalid choice. Please try again.');
+      }
+    }
+  }
+  
+function manageLeagues() {}
+function manageTeams(){}
+function managePlayers(){}
+function managePlayerStats(){}
+
+main();
